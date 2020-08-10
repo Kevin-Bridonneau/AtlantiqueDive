1ef4d6b1d1f670c0c3d90d6f61ec28542d2a69b1
Kevin Bridonneau <kevin.bridonneau@epitech.eu>
2020-08-10 17:21:42 +0200

    start working on dockerize the project

--- atlantiqueDiveApi/routes/api.php
+++ atlantiqueDiveApi/routes/api.php
@@ -18,10 +18,12 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// User routes
 Route::post('/login', 'UserController@login');
 Route::post('/register', 'UserController@register');
 Route::post('/updateProfile', 'UserController@update');
 
+// DiveSite route
 Route::post('/divesite', 'DiveSitesController@add');
 Route::get('/divesites', 'DiveSitesController@getAllDiveSites');
 
@@ -30,4 +32,8 @@ Route::get('/notice', 'DiveSitesController@getNotice');
 
 Route::post('/presence', 'DiveSitesController@addPresence');
 Route::get('/presence', 'DiveSitesController@getPresence');
+
+// Admin route
+
+Route::get('/users', 'AdminController@listUser');
 
